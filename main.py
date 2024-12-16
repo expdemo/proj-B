e8c29b00a44f67d91ec458fc3be3d8b8f22acfde
expdemo <devops.demo.srivalli@gmail.com>
2024-12-16 21:10:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 
 def main():
   org_repo = os.environ["ORG_REPO"]
-  token = os.environ["GITHUB_TOKEN"]
   pr_details = os.environ["PR_details"]
+  print("Hellooooo")
   print(org_repo)
-  print(token)
   print(pr_details)
   print("Hellooooo")
 
