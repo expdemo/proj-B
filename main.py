5681b22a0efe79055bc5897a3459ff6a47d7c705
expdemo <devops.demo.srivalli@gmail.com>
2024-12-16 22:37:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def main():
   org_repo = os.environ["ORG_REPO"]
-  pr_details = os.environ["PR_details"]
+  pr_details = os.getenv(PR_details)
   print("Hellooooo")
   print(org_repo)
   print(pr_details)
